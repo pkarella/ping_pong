f2a823a0b005a9d064e7abf974fb9b7c8abb422e
peter <pkarella27@yahoo.com>
2017-03-31 13:48:54 -0700

    continue to style and refractor

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
 
+function result(number){
+  for(var index = number; index <= number; index++){
+if(index % 5 === 0 && index % 3 === 0 ||index == 0) {
+  $("ul").append('<li>' + 'PING-PONG' + "</li>");
+  }
+ else if (index % 5 === 0 || index == 0) {
+  $("ul").append('<li>' +'PONG'+ "</li>");
+  }
+else if(index % 3 === 0 || index == 0) {
+  $("ul").append('<li>' +'PING'+ "</li>");
+  }
+else{
+  $("ul").append("<li>" + index + "</li>");
+  }
+
+};
+};
+
+
 
 
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-
     var input= parseInt($("#userNum").val());
-    var number=input;
-
-    for(var index =number; index<=number; index++){
-    if(index % 5 === 0 && index % 3 === 0) {
-      $("ul").append('<li>' + 'ping-pong' + "</li>");
-      }
-     else if (index % 5 === 0) {
-      $("ul").append('<li>' +'ping pong'+ "</li>");
-      }
-   else if(index % 3 === 0) {
-      $("ul").append('<li>' +'ping'+ "</li>");
-      }
-    else{
-      $("ul").append("<li>" + index + "</li>");
-      }
-
-};
-
+    var number= input;
+    if(0 >= number){
+    alert("Pick Another Number");
+  };
+  result(number);
 
   });
 });
