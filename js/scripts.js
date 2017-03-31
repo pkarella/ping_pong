d84a1dc01f4fa1c1cbf077bfac27c5e6052376e2
peter <pkarella27@yahoo.com>
2017-03-31 11:59:45 -0700

    continue to refractor javascript code. Add columns and panels

--- js/scripts.js
+++ js/scripts.js
@@ -2,40 +2,29 @@
 
 
 
-
-
-
-
-
-
-
-
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
 
-    var input= $("#userNum").val();
+    var input= parseInt($("#userNum").val());
     var number=input;
 
-    if( number % 3 && number % 5 ) {
-          $("ul").append('<li>' + number+ "</li>");
-      } else {
-          if( number % 3 === 0 && !number % 5 === 0 && !number % 15 === 0 ) {
-          $("ul").append('<li>' +'ping'+ "</li>");
-          }
-         if( number % 5 === 0 && !number % 3 === 0 && !number % 15 === 0 ) {
-          $("ul").append('<li>' +'pong'+ "</li>");
-          }
-        if ( number % 15 === 0 && !number % 5 === 0 && !number % 3 === 0) {
-          $("ul").append('<li>' +'ping pong'+ "</li>");
-        }
-    };
+    for(var index =number; index<=number; index++){
+    if(index % 5 === 0 && index % 3 === 0) {
+      $("ul").append('<li>' + 'ping-pong' + "</li>");
+      }
+     else if (index % 5 === 0) {
+      $("ul").append('<li>' +'ping pong'+ "</li>");
+      }
+   else if(index % 3 === 0) {
+      $("ul").append('<li>' +'ping'+ "</li>");
+      }
+    else{
+      $("ul").append("<li>" + index + "</li>");
+      }
+
+};
 
 
   });
 });
-
-
-
-
-// for (var x=number; x <= 100; x++){
